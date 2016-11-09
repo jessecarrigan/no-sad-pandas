8fdb9a1d5c2c853d415130a560b72cda48521fa0
Jesse Carrigan <jesse.m.carrigan@gmail.com>
2016-11-08 20:59:06 -0800

    Added mongo and delete method for testing.

--- nsp-schedule-api/mongo.js
+++ nsp-schedule-api/mongo.js
@@ -1,11 +1,12 @@
 var config = require('./config');
 var MongoClient = require('mongodb').MongoClient;
 
-var mongo = MongoClient.connect(config.mongoConnection, function(err, database) {
+var mongo;
+MongoClient.connect(config.mongoConnection, function(err, database) {
   if (err) {
     return console.log(err);
   }
-  return database;
+  mongo = database;
 });
 
 module.exports = mongo;
--- nsp-schedule-api/server.js
+++ nsp-schedule-api/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var validator = require('validator');
 var config = require('./config');
-var mongodb = require('./mongo');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 var app = express();
-
 app.use(bodyParser.json());
 
 // Launch app server
@@ -12,27 +12,36 @@ app.listen(3000, function() {
   console.log('listening on 3000');
 });
 
+var mongodb;
+MongoClient.connect(config.mongoConnection, function(err, database) {
+  if (err) {
+    return console.log(err);
+  }
+  mongodb = database;
+});
+
 // Get scheduled meetings.
 app.get('/meetings', function(req, res) {
-  db.collection('meetings').find().toArray(function(err, results) {
+  mongodb.collection('meetings').find().toArray(function(err, results) {
     if (err) {
       console.log(err);
       res.status(500).send({error: 'Internal server error'});      
     } else {
-      res.status(200).send(results);
+      res.status(200).send({meetings: results});
     }
   });
 });
 
 // Create a new meeting.
 app.post('/meeting', function(req, res) {
-  if (!req.body.email || !req.body.address || !req.body.contact || !req.body.datetime) {
+  if (!req.body.email || !req.body.address || !req.body.contact || !req.body.datetime || !req.body.timezone) {
     return res.status(400).send({error: 'Missing meeting information'});
   }
   // Validate email
   if (!validator.isEmail(req.body.email)) {
     return res.status(400).send({error: 'Invalid email'});
   }
+
   mongodb.collection('meetings').save(req.body, function(err, result) {
     if (err) {
       console.log(err);
@@ -45,17 +54,16 @@ app.post('/meeting', function(req, res) {
 
 // Update an existing meeting.
 app.put('/meeting/:id', function(req, res) {
-  mongodb.collection('meetings').save(req.body, function(err, result) {
-    if (err) {
-      console.log(err);
-      res.status(500).send({error: 'Internal server error'});
-    } else {
-      res.status(201).send(result);
-    }
-  });
 });
 
 // Delete a meeting.
 app.delete('/meeting/:id', function(req, res) {
-  res.send('Schedule Deleted');
+  mongodb.collection('meetings').deleteOne({_id: new ObjectId(req.params.id)}, function(err, result) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({error: 'Internal server error'});
+    } else {
+      return res.status(202).send(result);
+    }
+  });
 });
